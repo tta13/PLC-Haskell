38fb5edac2556dd1ba59702d9b62eea37105bb02
tta13 <tta@cin.ufpe.br>
2020-03-21 15:49:55 -0300

    Adiciona espaços :)

---
+++ Exercicios 1/Q1.hs
@@ -0,0 +1,8 @@
+addEspOnStr :: String -> Int -> String
+addEspOnStr [] _ = []
+addEspOnStr [a] _ = [a]
+addEspOnStr (a:as) c = a:[] ++ addEsp c ++ addEspOnStr as c
+
+addEsp :: Int -> String
+addEsp 0 = ""
+addEsp n = ' ':addEsp (n - 1)
