2ea78c6f216d2c92fd051cf11afc0b27a8eadf83
tta13 <tta@cin.ufpe.br>
2021-02-25 18:25:48 -0300

    Adicionando resolução para 'Árvore de Busca (b)' do Dikastis

--- Aulas/Aula 5/tree.hs
+++ Aulas/Aula 5/tree.hs
@@ -7,6 +7,10 @@ insert Nilt x                = Node x Nilt Nilt
 insert (Node n left right) x = if x >= n then (Node n left (insert right x)) 
                                else Node n (insert left x) right
 
+inOrder :: Ord t => Tree t -> [t]
+inOrder Nilt                = []
+inOrder (Node n left right) = inOrder left ++ n:inOrder right 
+
 insertList :: Ord t => Tree t -> [t] -> Tree t
 insertList tree []       = tree
 insertList (tree) (a:as) = insertList (insert tree a) as
@@ -14,6 +18,9 @@ insertList (tree) (a:as) = insertList (insert tree a) as
 buildBST :: Ord t => [t] -> Tree t
 buildBST []   = Nilt
 buildBST list = insertList Nilt list
+
+searchTreeSort :: Ord t => [t] -> [t]
+searchTreeSort list = inOrder (buildBST list)
 
 main = do
     a <- getLine
