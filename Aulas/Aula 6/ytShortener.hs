c4e4028afbce613fedc8761e956ba2bed3e12d17
tta13 <tta@cin.ufpe.br>
2021-03-01 22:20:46 -0300

    Removendo recursões redundantes

--- Aulas/Aula 6/ytShortener.hs
+++ Aulas/Aula 6/ytShortener.hs
@@ -6,16 +6,12 @@ shortYt = "https://youtu.be/"
 shorten :: String -> String
 shorten url = shortYt ++ (head (tail (splitOn splitter url)))
 
-shortenList :: [String] -> [String]
-shortenList []   = [""]
-shortenList (u:us) = shorten u:(shortenList us)
-
-putMultipleStr :: [String] -> IO ()
-putMultipleStr []     = putStr ""
-putMultipleStr (s:ss) = putStrLn s >> putMultipleStr ss
+shortenList :: [String] -> IO()
+shortenList []   = putStr ""
+shortenList (u:us) = putStrLn (shorten u) >> (shortenList us)
 
 showShortenedUrls :: String -> IO ()
-showShortenedUrls urls = putMultipleStr (shortenList (splitOn "\n" urls))
+showShortenedUrls urls = shortenList (splitOn "\n" urls)
 
 main :: IO ()
 main =
