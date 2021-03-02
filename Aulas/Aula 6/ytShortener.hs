3292a53a01e4182fd19ad6ee7366b8b97eeca272
tta13 <tta@cin.ufpe.br>
2021-03-01 22:15:45 -0300

    Removendo método desnecessário

--- Aulas/Aula 6/ytShortener.hs
+++ Aulas/Aula 6/ytShortener.hs
@@ -10,12 +10,9 @@ shortenList :: [String] -> [String]
 shortenList []   = [""]
 shortenList (u:us) = shorten u:(shortenList us)
 
-putLn :: String -> IO ()
-putLn str = putStr str >> putStr "\n"
-
 putMultipleStr :: [String] -> IO ()
 putMultipleStr []     = putStr ""
-putMultipleStr (s:ss) = putLn s >> putMultipleStr ss
+putMultipleStr (s:ss) = putStrLn s >> putMultipleStr ss
 
 showShortenedUrls :: String -> IO ()
 showShortenedUrls urls = putMultipleStr (shortenList (splitOn "\n" urls))
