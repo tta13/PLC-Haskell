af8966acdf60b905b2319456489a653960e83f89
tta13 <tta@cin.ufpe.br>
2021-03-01 22:13:04 -0300

    Lendo urls de arquivo e printando suas formas encurtadas

--- Aulas/Aula 6/ytShortener.hs
+++ Aulas/Aula 6/ytShortener.hs
@@ -6,11 +6,21 @@ shortYt = "https://youtu.be/"
 shorten :: String -> String
 shorten url = shortYt ++ (head (tail (splitOn splitter url)))
 
+shortenList :: [String] -> [String]
+shortenList []   = [""]
+shortenList (u:us) = shorten u:(shortenList us)
+
+putLn :: String -> IO ()
+putLn str = putStr str >> putStr "\n"
+
+putMultipleStr :: [String] -> IO ()
+putMultipleStr []     = putStr ""
+putMultipleStr (s:ss) = putLn s >> putMultipleStr ss
+
+showShortenedUrls :: String -> IO ()
+showShortenedUrls urls = putMultipleStr (shortenList (splitOn "\n" urls))
+
 main :: IO ()
 main =
-    putStrLn "Escrevendo" >>
-    writeFile "a.txt" "Hello\nworld" >>
-    appendFile "a.txt" "\nof\nHaskell"
-    >> putStrLn "Lendo o arquivo" >>
-    readFile "a.txt" >>=
-    \x -> putStrLn x
+    readFile "url.in" >>=
+    showShortenedUrls
