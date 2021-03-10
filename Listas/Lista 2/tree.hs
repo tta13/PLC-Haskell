2ad018f948d134ad558a08e9e7dc52d7a3fd1eb4
tta13 <tta@cin.ufpe.br>
2021-03-09 22:38:36 -0300

    Reaproveitando mais uma solução

--- Listas/Lista 2/tree.hs
+++ Listas/Lista 2/tree.hs
@@ -1,10 +1,14 @@
 data Tree t = Node t (Tree t) (Tree t) 
               | Nilt
-              deriving (Read)
+              deriving (Read, Show)
 
 height :: Tree t -> Int
 height (Nilt) = 0
 height (Node t (left) (right)) = 1 + max (height left) (height right)
+
+isBST :: Ord t => Tree t -> t -> t -> Bool
+isBST Nilt _ _                    = True
+isBST (Node n left right) min max = not(n < min) && not(n > max) && isBST left min n && isBST right n max
 
 main = do
        a <- getLine
