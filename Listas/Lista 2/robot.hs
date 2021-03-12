f3191c5f6626b3d80eee87efc9e243378279b16f
tta13 <tta@cin.ufpe.br>
2021-03-12 11:28:19 -0300

    Deixando resolução de 'Robo 1' mais concisa

--- Listas/Lista 2/robot.hs
+++ Listas/Lista 2/robot.hs
@@ -4,7 +4,7 @@ data Command = Forward Int | Backward Int | TurnLeft | TurnRight
 move :: (Int, Int) -> (Int, Int) -> Command -> (Int, Int)
 move (orX, orY) (posX, posY) (Forward n)  = (n*orX + posX, n*orY + posY)
 move (orX, orY) (posX, posY) (Backward n) = (-n*orX + posX, -n*orY + posY)
-
+move orientation position _ = position
 
 rotate :: (Int, Int) -> Command -> (Int, Int)
 rotate (orX, orY) TurnRight  = (orY, -orX)
@@ -13,10 +13,8 @@ rotate orientation _         = orientation
 
 goto :: (Int, Int) -> (Int, Int) -> [Command] -> (Int, Int)
 goto pos orientation []     = pos 
-goto pos orientation (x:xs) = 
-       case x of TurnLeft  -> goto pos (rotate orientation x) xs
-                 TurnRight -> goto pos (rotate orientation x) xs
-                 _         -> goto (move orientation pos x) orientation xs
+goto pos orientation (x:xs) = let newOri = rotate orientation x
+                              in goto (move newOri pos x) newOri xs 
 
 destination :: (Int,Int) -> [Command] -> (Int,Int)
 destination currentPosition cmds = goto currentPosition (0,1) cmds
